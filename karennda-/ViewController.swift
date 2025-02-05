b1198c73fdaeb36be59882410cf452f636a570c6
yukiikeda <superyuki0208@gmail.com>
2025-02-05 19:26:53 +0900

    Initial Commit

---
+++ karennda-/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  karennda-
+//
+//  Created by 池田友希 on 2025/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var headerPrevBtn: UIButton!//1
+    @IBOutlet weak var headerNextBtn: UIButton!//2
+    @IBOutlet weak var headerTitle: UILabel!  //3
+    @IBOutlet weak var calenderHeaderView: UIView! //4
+    @IBOutlet weak var calenderCollectionView: UICollectionView!//5
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    //①タップ時
+    @IBAction func tappedHeaderPrevBtn (sender: UIButton) {
+        
+        
+    //②タップ時
+    @IBAction func tappedHeaderNextBtn (sender: UIButton) {
+        }
+        
+        
+    }
