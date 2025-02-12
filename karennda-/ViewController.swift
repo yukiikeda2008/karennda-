6104ac7d3b711f8f46cd373fb7d3cf02bbd7b1b5
yukiikeda <superyuki0208@gmail.com>
2025-02-12 20:20:38 +0900

    今日の進捗です

--- karennda-/ViewController.swift
+++ karennda-/ViewController.swift
@@ -59,3 +59,65 @@ class ViewController: UIViewController {
                                                   
         }
     }
+
+import UIKit
+
+extension UIColor {
+    class func lightBlue() -> UIColor {
+        return UIColor (red: 92.0 / 255, green: 192.0 / 255, blue: 210,0 / 255, alpha: 1.0)
+    }
+    class func lightRed() -> UIColor {
+        return UIColor(red: 195.0 / 255, green: 123.0 / 255, blue: 175.0 / 255, alpha: 1.0)
+    }
+}
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
+    
+    let dateManager = dateManager()
+    let daysPerweed: Int = 7
+    let cellMargin: CGFloat = 2.0
+    var selectedDate = NSDate()
+    var today: NSDate!
+    let weekArray = ["Sun", "Mon", "Tue","Wed","Thu","Fri","Sat"]
+    
+    @IBOutlet weak var headerPrevBtn: UIButton!//①
+    @IBOutlet weak var headerNextBtu: UIButton!//②
+    @IBOutlet weak var headerTitle: UILabel!   //③
+    @IBOutlet weak var calenderHeaderView: UIView! //④
+    @IBOutlet weak var celenderCollectionView: UICollectionView! // ⑤
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        calenderCollectionView.delegate = self
+        calenderCollectionView.dataSource = self
+        calenderCollectionView.backgroundColor = UIColor.whiteColor()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated
+    }
+    
+    //1
+    func numberOfSections(collectionView: UICollectionView) -> Int {
+        return 2
+        
+    }
+    //2
+    func collectionView(collectionView: UICollectionView, umberOfItemsInSection section: Int) -> Int {
+        //Section毎にCellの総数を変える
+        if section == 0{
+            return 7
+        } else{
+            return dateManager.daysAcquisition()
+        }
+    }
+    //3
+    func collectionView(_ collectionView: UICollectionView, cellForItemArIndxPath indexpath: NSIndexPath) -> UICollectionViewCell{
+        
+        return cell
+    } 
+    <#code#>
+    }
